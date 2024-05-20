f837f2247cdf1fad60dcfb5def6799219fe70a13
Cybersport <Cybersport@26kadr.internal>
2024-05-20 15:52:29 +0300

    fdgdf

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
+from  keyboard.keyboards import get_keyboard_1,get_keyboard_2
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
 
 async def set_commands(bot: Bot):
     commands = [
@@ -15,7 +17,23 @@ async def set_commands(bot: Bot):
 
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.reply('Привет. я твой первый ЭХО бот')
+    await message.reply('Привет. я твой первый ЭХО бот',reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda message: message.text =='Отправь фото АК')
+async  def button_1_click(message:types.Message):
+    await bot.send_photo(message.chat.id, photo='https://upload.wikimedia.org/wikipedia/commons/f/fa/Ak74l.jpg?20130919161751',caption='Результат')
+
+@dp.message_handler(lambda message: message.text =='Перейти на следующую клавиатуру')
+async  def button_2_click(message:types.Message):
+    await message.answer('Перейти на следующую клавиатуру', reply_markup= get_keyboard_2())
+
+@dp.message_handler(lambda message: message.text =='Перейти на предыдущую клавиатуру')
+async  def button_2_click(message:types.Message):
+    await message.answer('Перейти на предыдущую клавиатуру', reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda message: message.text =='Отправь фото M4')
+async  def button_3_click(message:types.Message):
+    await bot.send_photo(message.chat.id, photo='https://upload.wikimedia.org/wikipedia/commons/d/dc/M4A1_ACOG.png',caption='Результат')
 
 @dp.message_handler(commands='help')
 async def help(message: types.Message):
